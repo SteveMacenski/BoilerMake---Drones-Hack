58ab16a35862528041c2b50a59a0764befa4fcec
Steven Macenski <stevenmacenski@gmail.com>
2015-10-17 20:23:52 -0400

    Finished ISS tracker ion file

--- ISS_tracker/ISS_tracker.ino
+++ ISS_tracker/ISS_tracker.ino
@@ -31,28 +31,18 @@ void loop() {
 while (Serial.available())  {
    altitude = Serial.parseFloat();
    asmuth = Serial.parseFloat();
+   Serial.flush();
+//   Serial.print(altitude);Serial.print(' ');Serial.println(asmuth);
 }
 
 //to ensure altitude is positive since servo is 0-180
 altitude = (int)altitude;
-if (altitude >= 0 && altitude <=180){
-  myServo.write(altitude);
-}
-if (altitude > 180 && altitude <360){
-  myServo.write(altitude - 180);
-}
+myServo.write(altitude+90);
 
 // defining variables
 double step_deg = 1.8; //degrees
 int step_count = 0; //number of steps taken
 
-//trial cases when rotating back around the 0-> 360 and 360->0 
-if (abs(asmuth_old-asmuth) > 300 && asmuth_old > 300) {
-  asmuth_old = asmuth_old - 360;
-}
-if (abs(asmuth_old-asmuth) > 300 && asmuth > 300) {
-  asmuth_old = asmuth_old + 360;
-}
 
 //calculating need for change in stepper motor 
 double asmuth_change = asmuth - asmuth_old;
@@ -63,13 +53,13 @@ int stepper_move = asmuth_change / step_deg;
 if (stepper_move > 1) {
   myMotor->step(stepper_move,FORWARD, SINGLE);
   if (stepper_move != 0){
-    Serial.println(stepper_move);
+//    Serial.println(stepper_move);
 }
 }
 if (stepper_move < -1) {
   myMotor->step(-1*stepper_move,BACKWARD, SINGLE);
   if (stepper_move != 0){
-    Serial.println(stepper_move);
+//    Serial.println(stepper_move);
   }
 }
 
